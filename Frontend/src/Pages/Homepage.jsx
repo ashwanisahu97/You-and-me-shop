17271d783745c7247d04b15a7f104bd2b7679718
ashwanisahu97 <ashsahu627@gmail.com>
2022-12-31 20:35:34 +0530

    add to acrt button added to homepage

--- Frontend/src/Pages/Homepage.jsx
+++ Frontend/src/Pages/Homepage.jsx
@@ -1,4 +1,4 @@
-import { Box, Image,Text,Flex } from '@chakra-ui/react';
+import { Box, Image,Text,Flex, Button } from '@chakra-ui/react';
 import React, { useEffect } from 'react'
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -19,12 +19,15 @@ const Homepage = () => {
         loading ? <Text color="red" m={ "auto"}>Data is loading</Text> : error ? <Text color="red" m={ "auto"}>Something went wrong,please try again</Text> :
           products?.map((element) => { 
             return (
-              <Box boxShadow={"rgba(100, 100, 111, 0.2) 0px 7px 29px 0px"} key={element.id} w={"20%"} mb={"15px"} borderRadius={ "10px"}>
+              <Box key={element.id} boxShadow={"rgba(100, 100, 111, 0.2) 0px 7px 29px 0px"}  w={"20%"} mb={"15px"} borderRadius={ "10px"}>
                 <Box h={ "300px"}><Image w={"100%"} h={ "100%"} src={element.image}></Image></Box>
-                <Box>
+                <Box fontSize={ "lg"} p={ "10px"} h={"150px"}>
                 <Text>{element.title }</Text>
-                <Text>{element.price }</Text>
+                <Text>${element.price }</Text>
                 </Box>
+               <Box>
+                  <Button _hover={{backgroundColor:"blue",color:"white"}} w={ "100%"} bg={ "red"}>Add to cart</Button>
+             </Box>
               </Box>
             )
           })
