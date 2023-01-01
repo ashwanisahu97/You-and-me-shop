50814cb578371c591218e753359f48a41ca80daa
ashwanisahu97 <ashsahu627@gmail.com>
2023-01-01 23:24:55 +0530

    product details page id fetched and showing on the ui

--- Frontend/src/Pages/ProductDetails.jsx
+++ Frontend/src/Pages/ProductDetails.jsx
@@ -1,10 +1,21 @@
 import { Box } from '@chakra-ui/react'
 import React from 'react'
-
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams} from "react-router-dom"
 const ProductDetails = () => {
+    const { id } = useParams();
+    const [singleProduct, setSingleProduct] = useState({})
+    const products=useSelector((store)=>store.data)
+    // useEffect(() => { 
+    //     const singleProd = products.find((e) => e.id == id)
+    //     setSingleProduct(singleProd)
+    // }, [id])
+    console.log("single product", singleProduct);
   return (
     <Box position={"absolute"} top={ "200px"}>
-    Hello,you are in Product details page
+   {` Hello,you id=${id} are in Product details page`}
    </Box>
   )
 }
