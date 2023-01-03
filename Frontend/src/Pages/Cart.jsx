34155e6398952579f945e47d790958d00b85d7fd
ashwanisahu97 <ashsahu627@gmail.com>
2023-01-04 00:09:35 +0530

    fetching data from cart

--- Frontend/src/Pages/Cart.jsx
+++ Frontend/src/Pages/Cart.jsx
@@ -1,7 +1,14 @@
 import { Box } from '@chakra-ui/react'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 
 const Cart = () => {
+  const dispatch = useDispatch();
+  const cartData=useSelector((store)=>cart.data)
+  useEffect(() => { 
+  dispatch(getFromCart())
+  }, [])
+  console.log("cartData", cartData);
   return (
       <Box position={"absolute"} top={ "200px"}>
     Hello,you are in cart page
