19c290dd7de4b23c474e64b1767f0498b7e08326
ashwanisahu97 <ashsahu627@gmail.com>
2023-01-03 22:19:28 +0530

    homepage styling updated

--- Frontend/src/Pages/Homepage.jsx
+++ Frontend/src/Pages/Homepage.jsx
@@ -18,20 +18,22 @@ const Homepage = () => {
   }, [dispatch,products?.length])
   console.log("products1",products)
   return (
-    <Grid gridGap={"20px"} templateColumns={"repeat(4,1fr)"} position={"relative"} top={ "100px"}>
+    <Grid gridGap={"30px"} templateColumns={"repeat(4,1fr)"} position={"relative"} top={ "100px"}>
       {
         loading ? <Text color="red" m={ "auto"}>Data is loading</Text> : error ? <Text color="red" m={ "auto"}>Something went wrong,please try again</Text> :
           products?.map((element) => { 
             return (
-              <Box key={element.id} boxShadow={"rgba(100, 100, 111, 0.2) 0px 7px 29px 0px"}  mb={"15px"} borderRadius={ "10px"}>
-                <Box _hover={{h:"350px",w:"300px"}} h={ "300px"}><Image w={"100%"} h={ "100%"} src={element.image}></Image></Box>
+              <Box onClick={() => {
+                navigate(`/productDetails/${element.id}`)
+              }} key={element.id} boxShadow={"rgba(100, 100, 111, 0.2) 0px 7px 29px 0px"} mb={"15px"} borderRadius={"10px"}>
+                <Box h={ "300px"}><Image w={"100%"} h={ "100%"} src={element.image}></Image></Box>
                 <Box fontSize={ "lg"} p={ "10px"} h={"150px"}>
                 <Text>{element.title }</Text>
                 <Text>${element.price }</Text>
                 </Box>
-               <Box>
+               {/* <Box>
                   <Button onClick={() => { navigate(`/productDetails/${element.id}`)}} _hover={{backgroundColor:"blue",color:"white"}} w={ "100%"} bg={ "red"}>See more details</Button>
-             </Box>
+             </Box> */}
               </Box>
             )
           })
