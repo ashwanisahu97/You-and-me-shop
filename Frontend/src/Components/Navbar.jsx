9cdcb216665a1c414bc5d81c6c8b015710e01e22
ashwanisahu97 <ashsahu627@gmail.com>
2023-01-08 01:54:22 +0530

    wishlist page:drawer functionality added

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,21 +1,33 @@
 import { Box, Flex, Image, Input,MenuButton,MenuItem,MenuList,Menu,useDisclosure, Button, Text} from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 import { AiOutlineSearch } from "react-icons/ai"
 import { BsCartFill} from "react-icons/bs"
 import { AiOutlineHeart } from "react-icons/ai"
 import { useDispatch, useSelector } from 'react-redux'
 import { getFromCart } from '../Redux/Cart/action'
+import Wishlist from '../Pages/Wishlist'
 const Navbar = () => {
   // const { isOpen, onOpen, onClose } = useDisclosure();
   const dispatch = useDispatch();
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const btnRef = React.useRef()
   const cartData=useSelector((store)=>store.cart.cart)
   useEffect(() => { 
   dispatch(getFromCart())
   }, [])
   console.log("cart data", cartData);
   return (
-      <Flex fontWeight="bold" fontSize={"xl"} position="fixed" zIndex={"1"} gap="30px" alignItems={"center"} bg="#f7f8f7" w={"100%"}>
+      <Flex fontSize={"xl"} position="fixed" zIndex={"1"} gap="30px" alignItems={"center"} bg="#f7f8f7" w={"100%"}>
           <Link to={"/"}><Image w={ "100"} h="100" src="https://i.pinimg.com/originals/ce/56/99/ce5699233cbc0f142250b520d967dff7.png"></Image></Link>
           <Box><Link to="/men">Men</Link> </Box>
           <Box><Link to="/women">Women</Link></Box>
@@ -31,7 +43,47 @@ const Navbar = () => {
         <Box>
        <Link to="/cart"><BsCartFill /></Link>
           </Box>
-        <Box><Link to="/wishlist"><AiOutlineHeart/></Link></Box>
+        <Box> <Button ref={btnRef} colorScheme='teal'  onClick={onOpen}>
+        <AiOutlineHeart/>
+        </Button>
+        <Drawer
+        isOpen={isOpen}
+        placement='right'
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Your Wishlist</DrawerHeader>
+
+              <DrawerBody>
+                {
+                  cartData?.map((e) => {
+                    return (
+                      <Flex mb="10px" boxSizing='border-box' border={"1px solid red"}>
+                      <Box p="5px" w="50%" h="100px">
+                      <Image w="100%" h="100%" src={ e.image}></Image>
+                        </Box>
+                        <Box w="50%">
+                          <Text overflow={"hidden"} h="50px">{ e.title}</Text>
+                          <Text>Price:{ e.price}</Text>
+                          <Button bg="teal" color={"white"} _hover={{bg:"red",color:"white"}}>Add To Cart</Button>
+                        </Box>
+                      </Flex>
+                    )
+                  })
+                }
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant='outline' mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme='blue'>Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer></Box>
       </Flex>
    </Flex>
   )
--- Frontend/src/Pages/Wishlist.jsx
+++ Frontend/src/Pages/Wishlist.jsx
@@ -1,12 +1,47 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Button, Input, useDisclosure } from '@chakra-ui/react'
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from '@chakra-ui/react'
 import React from 'react'
 
-const Wishlist = () => {
+function Wishlist({ isOpen, onOpen, onClose,btnRef}) {
+ 
+
   return (
-    <Box position={"absolute"} top={ "200px"}>
-    Hello,you are in Wishlist page
-   </Box>
+    < Box position={"relative"} top={ "100px"}>
+      <Button ref={btnRef} colorScheme='teal' onClick={onOpen}>
+        Open
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement='right'
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Create your account</DrawerHeader>
+
+          <DrawerBody>
+            <Input placeholder='Type here...' />
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant='outline' mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme='blue'>Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </Box>
   )
 }
-
 export default Wishlist
