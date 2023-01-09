edc4aece3d4d40108d1d4ec63fcb9de26883940d
ashwanisahu97 <ashsahu627@gmail.com>
2023-01-09 23:13:50 +0530

    women.jsx:sorting  section updated

--- Frontend/src/Pages/Women.jsx
+++ Frontend/src/Pages/Women.jsx
@@ -3,10 +3,10 @@ import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchData } from '../Redux/Product/action';
 import ProductSimple from '../Components/ProductSimple';
-import { Grid, Text } from '@chakra-ui/react';
+import { Box, Flex, Grid, Select, Text } from '@chakra-ui/react';
 const Men = () => {
   const dispatch = useDispatch();
-  const [womensData,setwomensData]=useState([])
+  const [womensData,setWomensData]=useState([])
   const loading=useSelector((store)=>store.product.loading)
   const products=useSelector((store)=>store.product.data)
   const error=useSelector((store)=>store.product.error)
@@ -19,10 +19,32 @@ const Men = () => {
       return element.category == "womens";
     })
     console.log("womensWear",womensWear)
-    setwomensData(womensWear)
-  },[products.length])
+    setWomensData(womensWear)
+  }, [products.length])
+  
+  const sortbyPrice = (e) => { 
+    let sortValue = e.target.value;
+    console.log(sortValue);
+    let temp = [...womensData];
+    if (sortValue === "lth") {
+      temp.sort((a,b)=>a.price-b.price);
+    } else { 
+      temp.sort((a,b)=>b.price-a.price)
+    }
+    setWomensData(temp);
+  }
  
   return (
+    <Box>
+      <Flex zIndex={"20"} pt="50px" justifyContent="center" alignItems="center">
+          <Box pt="7px" position={"fixed"} zIndex={"10"}>
+            <Select fontSize={"22px"} onChange={sortbyPrice}>
+              <option>Select by price</option>
+              <option value="lth">low to high</option>
+              <option value="htl">high to low</option>
+            </Select>
+      </Box>
+          </Flex>
     <Grid gridGap={"30px"} templateColumns={"repeat(5,1fr)"} position={"relative"} top={"100px"}>
     {
       loading ? <Text w={"200px"} mx="auto" color="red">Data is loading </Text> : error ? <Text color="red" mx={"auto"}>Something went wrong,please try again</Text> :
@@ -32,7 +54,8 @@ const Men = () => {
           )
         })
     }
-    </Grid>
+      </Grid>
+      </Box>
   )
   
 }
