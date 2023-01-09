574713d1ce760f672725b7208a235745d591b735
ashwanisahu97 <ashsahu627@gmail.com>
2023-01-09 13:32:09 +0530

    onchange added and sort value is getting

--- Frontend/src/Pages/Men.jsx
+++ Frontend/src/Pages/Men.jsx
@@ -4,11 +4,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchData } from '../Redux/Product/action';
 import { useNavigate } from 'react-router-dom';
 import ProductSimple from '../Components/ProductSimple';
-import { Grid, Text } from '@chakra-ui/react';
+import { Box, Flex, Grid, Select, Text} from '@chakra-ui/react';
 
 const Men = () => {
   const dispatch = useDispatch();
-  const [mensData,setMensData]=useState([])
+  const [mensData, setMensData] = useState([])
+  const [sort, setSort] = useState("");
   const loading=useSelector((store)=>store.product.loading)
   const products=useSelector((store)=>store.product.data)
   const error=useSelector((store)=>store.product.error)
@@ -22,20 +23,21 @@ const Men = () => {
     })
     console.log("mensWear",mensWear)
     setMensData(mensWear)
-  },[products.length])
-  
+  }, [products.length])
+  useEffect(() => { },[])
+  console.log("sort value", sort);
   return (
    <Box>
     <Flex zIndex={"20"} pt="30px" border={"2px solid green"} ml="600px" alignItems="center">
  <Flex>
           <Box pt="7px" position={"fixed"} zIndex={"10"}>
             <Select onChange={ (e)=>setSort(e.target.value)}>
-              <Option></Option>
-              <Option></Option>
-              <Option></Option>
+              <option>Select by price</option>
+              <option value="lth">low to high</option>
+              <option value="htl">high to low</option>
             </Select>
       </Box>
-          <Input position={"fixed"} border={"1px solid red"} zIndex={"10"} value={searchInput} onChange={(e) => setSearchInput(e.target.value)} fontSize={"xl"} pl={"30px"} type={"text"} placeholder={`What are you looking for`} w={"300px"}></Input>
+          {/* <Input position={"fixed"} border={"1px solid red"} zIndex={"10"} value={searchInput} onChange={(e) => setSearchInput(e.target.value)} fontSize={"xl"} pl={"30px"} type={"text"} placeholder={`What are you looking for`} w={"300px"}></Input> */}
           </Flex>
           </Flex>
     <Grid gridGap={"30px"} templateColumns={"repeat(4,1fr)"} position={"relative"} top={"100px"}>
