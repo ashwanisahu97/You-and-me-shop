56e4669cafd39af3b2289e6bcaf2cfe08fbb75b7
ashwanisahu97 <ashsahu627@gmail.com>
2023-01-09 11:28:27 +0530

    jewelery page updated

--- Frontend/src/Pages/Jewelery.jsx
+++ Frontend/src/Pages/Jewelery.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchData } from '../Redux/Product/action';
 import ProductSimple from '../Components/ProductSimple';
+import { Grid, Text } from '@chakra-ui/react';
 
 const Jewelery = () => {
   const dispatch = useDispatch();
@@ -23,7 +24,16 @@ const Jewelery = () => {
   },[products.length])
   
   return (
-    <ProductSimple data={jeweleryData} loading={loading} error={error} />
+    <Grid gridGap={"30px"} templateColumns={"repeat(4,1fr)"} position={"relative"} top={"100px"}>
+    {
+      loading ? <Text w={"200px"} mx="auto" color="red">Data is loading </Text> : error ? <Text color="red" mx={"auto"}>Something went wrong,please try again</Text> :
+        jeweleryData?.map((element) => {
+          return (
+            <ProductSimple element={element} key={ element.id} />
+          )
+        })
+    }
+    </Grid>
   )
   
 }
