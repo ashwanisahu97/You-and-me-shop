54b167981bc4bc47d512f853eeaf7395488fe32e
ashwanisahu97 <ashsahu627@gmail.com>
2023-01-09 11:27:10 +0530

    women page updated

--- Frontend/src/Pages/Women.jsx
+++ Frontend/src/Pages/Women.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchData } from '../Redux/Product/action';
 import ProductSimple from '../Components/ProductSimple';
+import { Grid, Text } from '@chakra-ui/react';
 const Men = () => {
   const dispatch = useDispatch();
   const [womensData,setwomensData]=useState([])
@@ -22,7 +23,16 @@ const Men = () => {
   },[products.length])
  
   return (
-    <ProductSimple data={womensData} loading={loading} error={error} />
+    <Grid gridGap={"30px"} templateColumns={"repeat(4,1fr)"} position={"relative"} top={"100px"}>
+    {
+      loading ? <Text w={"200px"} mx="auto" color="red">Data is loading </Text> : error ? <Text color="red" mx={"auto"}>Something went wrong,please try again</Text> :
+        womensData?.map((element) => {
+          return (
+            <ProductSimple element={element} key={ element.id} />
+          )
+        })
+    }
+    </Grid>
   )
   
 }
