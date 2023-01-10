47a787f09bd513f9b5ec55f8f91989a7cc208ca7
ashwanisahu97 <ashsahu627@gmail.com>
2023-01-10 22:34:27 +0530

    actionTypes defined for wishlist

--- Frontend/src/Redux/Wishlist/action.js
+++ Frontend/src/Redux/Wishlist/action.js
@@ -1,2 +1,22 @@
+import axios from "axios";
 import * as Types from "./actionTypes"
-export const
+export const addToWishlistRequest = () => { 
+    return {
+        type:Types.ADD_TO_WISHLIST_REQUEST
+    }
+}
+export const addToWishlistSuccess = (payload) => { 
+    return {
+        type: Types.ADD_TO_WISHLIST_REQUEST,
+        payload
+    }
+}
+export const addToWishlistFailure = () => { 
+    return {
+        type:Types.ADD_TO_WISHLIST_REQUEST
+    }
+}
+
+export const addToWishlist = () => () => { 
+      axios.get("/")
+}
