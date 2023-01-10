2fa91cb8497bd9aaf960ee3600b8e19756835392
ashwanisahu97 <ashsahu627@gmail.com>
2023-01-10 22:43:36 +0530

    function for add to wishlist  defined for wishlist

--- Frontend/src/Redux/Wishlist/reducer.js
+++ Frontend/src/Redux/Wishlist/reducer.js
@@ -4,4 +4,26 @@ const initState = {
     wishlist: [],
     error:false
 }
-export const reducer = (state = initState, {type,payload})
+export const reducer = (state = initState, { type, payload }) => { 
+    switch (type) { 
+        case Types.ADD_TO_WISHLIST_REQUEST:
+            return {
+                ...state,
+                loading:true
+            }
+        case Types.ADD_TO_WISHLIST_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                wishlist:payload
+            }
+        case Types.ADD_TO_WISHLIST_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                error:true
+            }
+        default:
+            return state;
+    }
+}
